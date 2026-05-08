ccdbb8125ae895b446a852818f383289eba0d08f
JadielSilva11 <jdielcunha@gmail.com>
2026-05-08 16:03:38 -0300

    Adicionando comentários no código

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Função que verifica o fim das expressões (do, while)
 int check_end(char next){
     char end[] = {' ', '{', '}', '(', ')', ';', '[', ']'};
     for(int i = 0; i < 8; i++){
@@ -14,6 +15,7 @@ int check_end(char next){
     return 0;
 }
 
+//Função que verifica se os identificadores são escirtos corretamente seguindo as normas da linguagem C
 int check_id(string s){
 
     if(s[0] != '_' && (s[0] < 'a' || s[0] > 'z') && (s[0] < 'A' || s[0] > 'Z')){
@@ -30,6 +32,7 @@ int check_id(string s){
     return 1;
 }
 
+//Função que identifica um operador relacional
 int check_op(char c){
     if(c >= '<' && c <= '>'){
         return 1;
@@ -71,35 +74,35 @@ int main(){
 
                 // Verificação de palavras reservadas e identificadores com base em suas características.
 
-                //Verificação do DO
+                //Verificação do 'do'
                 if(buffer_s == "do" && check_end(buffer_next)){
                     lexemas.push_back(buffer_s);
                     tokens.push_back(token_syntax[0]);
                     cont++;
                     buffer_s = "";
                 }
-                //Verificação do {
+                //Verificação do '{'
                 else if(buffer_s == "{"){
                     lexemas.push_back(buffer_s);
                     tokens.push_back(token_syntax[1]);
                     cont++;
                     buffer_s = "";
                 }
-                //Verificação do }
+                //Verificação do '}'
                 else if(buffer_s == "}"){
                     lexemas.push_back(buffer_s);
                     tokens.push_back(token_syntax[2]);
                     cont++;
                     buffer_s = "";
                 }
-                //Verificação do WHILE
+                //Verificação do 'while'
                 else if(buffer_s == "while" && check_end(buffer_next)){
                     lexemas.push_back(buffer_s);
                     tokens.push_back(token_syntax[3]);
                     cont++;
                     buffer_s = "";
                 }
-                //Verificação do (
+                //Verificação do '('
                 else if(buffer_s == "("){
                     lexemas.push_back(buffer_s);
                     tokens.push_back(token_syntax[4]);
@@ -107,7 +110,7 @@ int main(){
                     buffer_s = "";
                 }
 
-                //Verificação do )
+                //Verificação do ')'
                 else if(buffer_s == ")"){
                     lexemas.push_back(buffer_s);
                     tokens.push_back(token_syntax[8]);
@@ -115,7 +118,7 @@ int main(){
                     buffer_s = "";
                 }
 
-                //Verificação do ;
+                //Verificação do ';'
                 else if(buffer_s == ";"){
                     lexemas.push_back(buffer_s);
                     tokens.push_back(token_syntax[9]);
@@ -123,7 +126,7 @@ int main(){
                     buffer_s = "";
                 }
 
-                //Verificação do OPERADOR RELACIONAL
+                //Verificação dos operadores '<, =, >'
                 else if(check_op(buffer_c) && ((buffer_next >= 'a' && buffer_next <= 'z') || buffer_next == ' ')){
                     lexemas.push_back(buffer_s);
                     tokens.push_back(token_syntax[6]);
@@ -131,7 +134,7 @@ int main(){
                     buffer_s = "";
                 }
 
-                //Verificação do IDENTIFICADOR
+                //Verificação do 'identificador'
                 else if(check_end(buffer_next)){
                     if(!check_id(buffer_s)){
                         return 1;
@@ -153,6 +156,7 @@ int main(){
 
     // Analisador Sintático
 
+    //Verifica se o código recebido foi digitado na ordem correta. Se não, exibe msg de erro e aponta onde está o erro
     for(int i = 0; i < 10; i++){
         if(token_syntax[i] != tokens[i]){
             cout << "Erro sintático (expressão fora de ordem ou incompleta) -> " + lexemas[i] + " " + lexemas[i + 1] + "\n";
